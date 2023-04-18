2c91a1f1f0b4ee7c88f3d6e9b93c73bbd231046e
Ilianikonov <nikonov.ilia2013@yandex.ru>
2023-04-18 18:30:44 +0300

    Create Test

--- src/main/java/com/example/demo/controller/RestController.java
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -14,6 +14,7 @@ import java.util.Date;
 import java.util.List;
 
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping("/rest/players")
 public class RestController {
     private  final PlayerService playerService;
 
@@ -22,27 +23,27 @@ public class RestController {
         this.playerService = playerService;
     }
 
-    @GetMapping("/rest/players")
-    public List<PlayersResponse> getPlauersList(@RequestParam(required = false) String name,
-                                       @RequestParam(required = false) String title,
-                                       @RequestParam(required = false) Race race,
-                                       @RequestParam(required = false) Profession profession,
-                                       @RequestParam(required = false) Long after,
-                                       @RequestParam(required = false) Long before,
-                                       @RequestParam(required = false) Boolean banned,
-                                       @RequestParam(required = false) Integer minExperience,
-                                       @RequestParam(required = false) Integer maxExperience,
-                                       @RequestParam(required = false) Integer minLevel,
-                                       @RequestParam(required = false) Integer maxLevel,
-                                       @RequestParam(defaultValue = "ID") PlayerOrder order,
-                                       @RequestParam(defaultValue = "0") Integer pageNumber,
-                                       @RequestParam(defaultValue = "3") Long pageSize){
+    @GetMapping
+    public List<PlayersResponse> getPlayersList(@RequestParam(required = false) String name,
+                                                @RequestParam(required = false) String title,
+                                                @RequestParam(required = false) Race race,
+                                                @RequestParam(required = false) Profession profession,
+                                                @RequestParam(required = false) Long after,
+                                                @RequestParam(required = false) Long before,
+                                                @RequestParam(required = false) Boolean banned,
+                                                @RequestParam(required = false) Integer minExperience,
+                                                @RequestParam(required = false) Integer maxExperience,
+                                                @RequestParam(required = false) Integer minLevel,
+                                                @RequestParam(required = false) Integer maxLevel,
+                                                @RequestParam(defaultValue = "ID") PlayerOrder order,
+                                                @RequestParam(defaultValue = "0") Integer pageNumber,
+                                                @RequestParam(defaultValue = "3") Long pageSize){
         PlayerFilter playerFilter = new PlayerFilter(name, title, race, profession, after, before, banned, minExperience, maxExperience, minLevel, maxLevel, order, pageNumber, pageSize);
         return  playerService.getPlayers(playerFilter).stream()
                 .map(this::convertToPlayerResponse)
                 .toList();
     }
-    @GetMapping("/rest/players/count")
+    @GetMapping("/count")
     public int getPlayersCount(@RequestParam(required = false) String name,
                                @RequestParam(required = false) String title,
                                @RequestParam(required = false) Race race,
@@ -56,7 +57,7 @@ public class RestController {
                                @RequestParam(required = false) Integer maxLevel){
         return playerService.getPlayersCount(new PlayerFilter(name,title,race,profession,after,before,banned,minExperience,maxExperience,minLevel,maxLevel, PlayerOrder.ID, 0, Long.MAX_VALUE));
     }
-    @PostMapping("/rest/players")
+    @PostMapping
     public PlayersResponse createPlayer (@RequestBody CreatePlayerRequest createPlayerRequest){
         Player player = new Player();
         player.setName(createPlayerRequest.getName());
@@ -68,15 +69,15 @@ public class RestController {
         player.setBanned(createPlayerRequest.getBanned());
         return convertToPlayerResponse(playerService.createPlayer(player));
     }
-    @GetMapping("/rest/players/{id}")
+    @GetMapping("/{id}")
     public PlayersResponse getPlayer(@PathVariable Long id){
         return convertToPlayerResponse(playerService.getPlayerById(id));
     }
-    @PostMapping("/rest/players/{id}")
+    @PostMapping("/{id}")
     public PlayersResponse updatePlayer (@PathVariable Long id, @RequestBody CreatePlayerRequest createPlayerRequest){
         return convertToPlayerResponse(playerService.updatePlayer(id,createPlayerRequest));
     }
-    @DeleteMapping("/rest/players/{id}")
+    @DeleteMapping("/{id}")
     public void deletePlayer (@PathVariable Long id){
         playerService.deletePlayer(id);
     }
